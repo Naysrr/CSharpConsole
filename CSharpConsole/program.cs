87cfc650f18a628291d3e32c64a29b2a145f583e
Yanis <yayaezzannati@gmail.com>
2023-03-07 10:43:35 +0100

    modif code tc

--- CSharpConsole/program.cs
+++ CSharpConsole/program.cs
@@ -1,4 +1,3 @@
-// See https://aka.ms/new-console-template for more information
 class Program
 {
 
@@ -12,14 +11,27 @@ class Program
     {
         string[] month = new string[] { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre" };
         string closedMonth = "Aout";
-        double tauxPrime = 0.10;
+        double tauxPrime = 0;
 
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine("Quel est votre Salaire annuel Brut : ");
         bool salaryInInt = int.TryParse(Console.ReadLine().Replace("€", ""), out int salary);
         Console.WriteLine("\nQuel est votre Taux d'imposition : ");
         double taxes = double.Parse(Console.ReadLine().Replace("%", ""));
-        Console.WriteLine("\nVous avez un salaire de : " + salary + "€ Brut" + "\nImposable a " + taxes + "%");
+        Console.WriteLine("\nQuel est le Taux de la prime de fin d'année : ");
+        try
+        {
+            tauxPrime = double.Parse(Console.ReadLine().Replace("%", ""));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Le taux de la Prime de fin d'année doit être un entier");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("La division ne peux pas être par 0");
+        }
+        Console.WriteLine("\nVous avez un salaire de : " + salary + "€ Brut" + "\nImposable a " + taxes + "%" + "\n avec une prime de fin d'année de : " + tauxPrime + "%");
         double salaryNet = Math.Round(calculateSalary(salary, taxes), 2);
         Console.WriteLine("\nVous gagnez donc : " + salaryNet + "€ Net");
         switch (salary)
